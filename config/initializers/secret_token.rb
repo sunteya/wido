d3fa7130de4d098fbaeb6f6b0c19cdf03d6b0192
sunteya <sunteya@gmail.com>
2013-02-18 21:34:06 +0800

    初始化项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wido2::Application.config.secret_token = '491646a441aca2e9fc888ecc1d799fe89ebd10b9367f45c7a9967b47caecf03b5173fef51e92eb38667507949fb32cd328e7965c09a73c883dd6d95a68777dac'
