fd9d0931cc84841912d647652387aacd98eaefff
sunteya <sunteya@gmail.com>
2013-11-21 13:42:33 +0800

    更新发布脚本

--- config/deploy/wido.me.rb
+++ config/deploy/wido.me.rb
@@ -1,6 +1,5 @@
-set :deploy_to, "/var/www/wido.next/#{application}"
+set :deploy_to, "/var/www/wido/apps/#{application}"
 
 set :user, "www-data"
 set :rails_env, :production
-server "next.wido.me", :app, :web, :db, :primary => true , :whenever => true
-# set :asset_env, "RAILS_RELATIVE_URL_ROOT=/mportal"
+server "wido.me", :app, :web, :db, :primary => true , :whenever => true
