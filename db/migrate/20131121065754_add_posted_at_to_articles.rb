811b462e15feae826741150f891f3163f861b43c
sunteya <sunteya@gmail.com>
2013-11-22 09:15:53 +0800

    修正迁移脚本

--- db/migrate/20131121065754_add_posted_at_to_articles.rb
+++ db/migrate/20131121065754_add_posted_at_to_articles.rb
@@ -2,6 +2,7 @@ class AddPostedAtToArticles < ActiveRecord::Migration
   def change
     add_column :articles, :posted_at, :datetime
 
+    Article.reset_column_information
     Article.find_each do |article|
       article.posted_at = article.revised_at || article.published_at
       article.save
