8d40125b2ff90aa983ad0c4913a5ebf238106b02
sunteya <sunteya@gmail.com>
2013-12-04 08:40:55 +0800

    增加附件加载链接

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,25 @@ protected
     redirect_to params[:ok_url] || default
   end
 
+  def public_url(source)
+    if source.include?("://")
+      source
+    else
+      "#{request.protocol}#{request.host_with_port}#{public_path(source)}"
+    end
+  end
+
+  def public_path(source)
+    url = url_for(source)
+    return url if url.include?("://")
+    
+    if url.start_with?(request.script_name)
+      url
+    else
+      "#{request.script_name}#{url}"
+    end
+  end
+
+  helper_method :public_path, :public_url
+
 end
