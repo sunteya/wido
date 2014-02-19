96d265b1ba3218c23b0d28afb5fd3299e9517898
sunteya <sunteya@gmail.com>
2014-02-19 10:29:07 +0800

    gem 安装到项目目录下

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 require "capsum/typical"
 
 set :application, "wido2"
+set :bundle_dir, -> { File.join(shared_path, 'bundle') }
 
 set :shared, %w{
   config/database.yml
--- config/deploy/wido.me.rb
+++ config/deploy/wido.me.rb
@@ -2,4 +2,4 @@ set :deploy_to, "/var/www/wido/apps/#{application}"
 
 set :user, "www-data"
 set :rails_env, :production
-server "wido.me", :app, :web, :db, :primary => true , :whenever => true
+server "wido.me", :app, :web, :db, :primary => true
